973345feea95e001ea3f43f0ad93d98269789518
florin <fofo.munteanu@gmail.com>
2021-03-16 09:10:18 +0200

    print raport livrari soferi

--- templates_c/f2b86ffce0eb1085a87d6fdf07c6087890100747.file.raport_livrari_soferi.tpl.php
+++ templates_c/f2b86ffce0eb1085a87d6fdf07c6087890100747.file.raport_livrari_soferi.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.15, created on 2021-03-16 09:04:11
+<?php /* Smarty version Smarty-3.1.15, created on 2021-03-16 09:09:55
          compiled from "/var/www/html/fofoweb/www/templates/raport_livrari_soferi.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:181507985860227fc6e3c330-67745625%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'f2b86ffce0eb1085a87d6fdf07c6087890100747' => 
     array (
       0 => '/var/www/html/fofoweb/www/templates/raport_livrari_soferi.tpl',
-      1 => 1615878245,
+      1 => 1615878592,
       2 => 'file',
     ),
   ),
@@ -45,7 +45,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
         <div class="wrapper">
             <div class="container-fluid">
                 <div id="heading" class="page-header">
-                    <h1><i class="icon20 i-menu-6"></i> Raport livrari sofer (de verificat cantitatile + print TO DO)
+                    <h1><i class="icon20 i-menu-6"></i> Raport livrari sofer
                         <a target="_blank" href="/print_raport_livrari_soferi.php?id=<?php echo $_smarty_tpl->tpl_vars['sofer_id']->value;?>
 &data_start=<?php echo $_smarty_tpl->tpl_vars['data_start']->value;?>
 &data_stop=<?php echo $_smarty_tpl->tpl_vars['data_stop']->value;?>
--- www/templates/raport_livrari_soferi.tpl
+++ www/templates/raport_livrari_soferi.tpl
@@ -7,7 +7,7 @@
         <div class="wrapper">
             <div class="container-fluid">
                 <div id="heading" class="page-header">
-                    <h1><i class="icon20 i-menu-6"></i> Raport livrari sofer (de verificat cantitatile + print TO DO)
+                    <h1><i class="icon20 i-menu-6"></i> Raport livrari sofer
                         <a target="_blank" href="/print_raport_livrari_soferi.php?id={$sofer_id}&data_start={$data_start}&data_stop={$data_stop}">
                             <button class="i-print"></button>
                         </a>
