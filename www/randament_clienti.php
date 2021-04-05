2d35018464676a7d6fdd5b75c45bdc06c087bf93
florin <fofo.munteanu@gmail.com>
2021-04-05 13:26:35 +0300

    randament clienti

--- www/randament_clienti.php
+++ www/randament_clienti.php
@@ -38,12 +38,9 @@ $smarty->assign('lista_perioade', $lista_perioade);
 $lista_ani = Calendar::getAni();
 $smarty->assign('lista_ani', $lista_ani);
 
-//debug('am an ' . $an  . ' and perioada_id ' . $perioada_id);
-
-//$conditie_update = '';
+$conditie_update = '';
 if (($an >= 2021 || ($an == 2020 && $perioada_id >=11))) {
-    debug('am an ' . $an  . ' and perioada_id ' . $perioada_id);
-    $conditie_update = "disabled";
+    $conditie_update = "readonly";
     $smarty->assign('conditie_update', $conditie_update);
 }
 
