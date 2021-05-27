fb262bca8a76f7ae94f6a8004a560faaffd6c923
florin <fofo.munteanu@gmail.com>
2021-05-27 09:54:11 +0300

    extra

--- www/adauga_produse_extra_fisa.php
+++ www/adauga_produse_extra_fisa.php
@@ -20,14 +20,8 @@ $smarty->assign('nume_client', $nume_client);
 
 $tip_produs_id = getRequestParameter('tip_produs_id', 0);
 $cantitate = getRequestParameter('cantitate', 0);
-
 $pret = getRequestParameter('pret', 0);
 
-$stare_produs = getRequestParameter('stare_produs', 1);
-$smarty->assign('stare_produs', $stare_produs);
-
-$lista_stari_produse = Produse::getStareProdus();
-$smarty->assign('lista_stari_produse', $lista_stari_produse);
 
 $lista_tip_stoc = Produse::getTipProdus();
 $smarty->assign('lista_tip_stoc', $lista_tip_stoc);
@@ -35,9 +29,6 @@ $smarty->assign('lista_tip_stoc', $lista_tip_stoc);
 if ($adauga) {
     Fise::AdaugaProduseExtraFisa($fisa_id, $client_id);
 }
-
-//$produse_extra = Fise::getProduseExtraByFisaIdAndClientId($fisa_id, $client_id);
-//$smarty->assign('produse_extra', $produse_extra);
 
 $produse_extra = Fise::getProduseExtraByFisaIdAndClientId($fisa_id, $client_id);
 $smarty->assign('produse_extra', $produse_extra);
