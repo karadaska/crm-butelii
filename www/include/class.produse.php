33d7a185ee8f1acf35aaa3fd371737e337d24cc5
florin <fofo.munteanu@gmail.com>
2021-06-10 22:56:48 +0300

    raport livrari depozit

--- www/include/class.produse.php
+++ www/include/class.produse.php
@@ -269,6 +269,39 @@ class Produse
 
     }
 
+    public static function getTotalCantitatiByDepozitIdAndProdusId($depozit_id = 0, $tip_produs_id, $opts = array())
+    {
+        $ret = null;
+        $data_start = isset($opts['data_start']) ? $opts['data_start'] : 0;
+        $data_stop = isset($opts['data_stop']) ? $opts['data_stop'] : 0;
+
+        if ($data_start == 0) {
+            $data_start = date('Y-m-01');
+        }
+
+        if ($data_stop == 0) {
+            $data_stop = date('Y-m-t');
+        }
+
+        $target_by_client_id = "SELECT SUM(a.cantitate) as cantitate, SUM(a.cantitate * a.pret) as valoare,  SUM(a.comision) as comision
+                                FROM detalii_fisa_intoarcere_produse  as a
+                                LEFT JOIN fise_generate as b on a.fisa_id = b.id
+                                WHERE b.depozit_id = '" . $depozit_id . "'                                
+                                AND a.tip_produs_id = '" . $tip_produs_id . "'                                
+                                AND a.data_intrare >= '" . $data_start . "'
+                                AND a.data_intrare <= '" . $data_stop . "'
+                                AND a.sters = 0
+                                AND a.cantitate > 0
+                                ";
+
+        $result = myQuery($target_by_client_id);
+        if ($result) {
+            $ret = $result->fetch(PDO::FETCH_ASSOC);
+        }
+        return $ret;
+
+    }
+
     public static function getTotalCantitatiByDepozitIdAndMasinaIdAndTraseuIdAndSoferId($depozit_id, $masina_id = 0, $traseu_id, $sofer_id, $opts = array())
     {
         $ret = null;
